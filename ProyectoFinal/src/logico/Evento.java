d30352a267f31058cdb6e40470aaa59ccfa3d162
Esmery <Esmery@LAPTOP-THV97>
2024-11-21 15:46:46 -0400

    Creando clase evento

---
+++ ProyectoFinal/src/logico/Evento.java
@@ -0,0 +1,22 @@
+package logico;
+
+
+public class Evento {
+	private String codEvento;
+	private String nombreEvento;
+	
+	public String getCodEvento() {
+		return codEvento;
+	}
+	public void setCodEvento(String codEvento) {
+		this.codEvento = codEvento;
+	}
+	public String getNombreEvento() {
+		return nombreEvento;
+	}
+	public void setNombreEvento(String nombreEvento) {
+		this.nombreEvento = nombreEvento;
+	}
+	
+
+}
