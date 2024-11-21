f237011aeee385d300ea88d9da2e55ebf30b5aa2
USER <USER@DESKTOP-FDVMDIV>
2024-11-21 15:35:16 -0400

    Agregando atributos de clase Recurso

--- ProyectoFinal/src/logico/Recurso.java
+++ ProyectoFinal/src/logico/Recurso.java
@@ -2,4 +2,13 @@ package logico;
 
 public class Recurso {
 
+	private String codRecurso;
+
+	public String getCodRecurso() {
+		return codRecurso;
+	}
+
+	public void setCodRecurso(String codRecurso) {
+		this.codRecurso = codRecurso;
+	}
 }
