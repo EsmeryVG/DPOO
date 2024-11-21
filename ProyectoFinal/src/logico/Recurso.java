3a5e2652cad81711616894fd3fba54a6ef4816e4
USER <USER@DESKTOP-FDVMDIV>
2024-11-21 15:41:10 -0400

    Atributos de Recurso listo

--- ProyectoFinal/src/logico/Recurso.java
+++ ProyectoFinal/src/logico/Recurso.java
@@ -3,6 +3,9 @@ package logico;
 public class Recurso {
 
 	private String codRecurso;
+	private String tipoRecurso;
+	private String nombreRecurso;
+	private String disponibilidadRecurso;
 
 	public String getCodRecurso() {
 		return codRecurso;
@@ -11,4 +14,30 @@ public class Recurso {
 	public void setCodRecurso(String codRecurso) {
 		this.codRecurso = codRecurso;
 	}
+
+	public String getTipoRecurso() {
+		return tipoRecurso;
+	}
+
+	public void setTipoRecurso(String tipoRecurso) {
+		this.tipoRecurso = tipoRecurso;
+	}
+
+	public String getNombreRecurso() {
+		return nombreRecurso;
+	}
+
+	public void setNombreRecurso(String nombreRecurso) {
+		this.nombreRecurso = nombreRecurso;
+	}
+
+	public String getDisponibilidadRecurso() {
+		return disponibilidadRecurso;
+	}
+
+	public void setDisponibilidadRecurso(String disponibilidadRecurso) {
+		this.disponibilidadRecurso = disponibilidadRecurso;
+	}
+	
+	
 }
