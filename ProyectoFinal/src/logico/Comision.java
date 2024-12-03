f05b7bea1689639280e5dc90d998a22c8a5c22eb
Esmery <Esmery@LAPTOP-THV97>
2024-12-02 22:28:35 -0400

    Actualizando clase Comision

--- ProyectoFinal/src/logico/Comision.java
+++ ProyectoFinal/src/logico/Comision.java
@@ -1,20 +1,26 @@
 package logico;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 
-public class Comision {
+public class Comision implements Serializable{
 
+	private static final long serialVersionUID = 1L;
+	private String codigo;
 	private String nombre;
     private String descripcion;
-    private ArrayList<Persona> miembros;
+    private ArrayList<Jurado> miembros;
     private TrabajoCientifico miTrabajo;
+    public static int codigoComision = 1;
 
-    public Comision(String nombre, String descripcion, ArrayList<Persona> miembros, TrabajoCientifico miTrabajo) {
+    public Comision(String nombre, String descripcion, ArrayList<Jurado> miembros, TrabajoCientifico miTrabajo) {
 		super();
+		this.codigo = "COMISION-N"+codigoComision;
 		this.nombre = nombre;
 		this.descripcion = descripcion;
 		this.miembros = miembros;
 		this.miTrabajo = miTrabajo;
+		codigoComision++;
 	}
 
 	public String getNombre() {
@@ -33,11 +39,11 @@ public class Comision {
 		this.descripcion = descripcion;
 	}
 
-	public ArrayList<Persona> getMiembros() {
+	public ArrayList<Jurado> getMiembros() {
 		return miembros;
 	}
 
-	public void setMiembros(ArrayList<Persona> miembros) {
+	public void setMiembros(ArrayList<Jurado> miembros) {
 		this.miembros = miembros;
 	}
 
@@ -47,6 +53,22 @@ public class Comision {
 
 	public void setMiTrabajo(TrabajoCientifico miTrabajo) {
 		this.miTrabajo = miTrabajo;
+	}
+
+	public String getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+
+	public static int getCodigoComision() {
+		return codigoComision;
+	}
+
+	public static void setCodigoComision(int codigoComision) {
+		Comision.codigoComision = codigoComision;
 	}
 
 }
