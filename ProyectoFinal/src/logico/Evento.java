d26234a548c21f7f4bf3ddd731649bb42c127665
USER <USER@DESKTOP-FDVMDIV>
2024-12-02 22:16:25 -0400

    Actualizando clases Evento y Jurado

--- ProyectoFinal/src/logico/Evento.java
+++ ProyectoFinal/src/logico/Evento.java
@@ -1,22 +1,114 @@
 package logico;
 
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
 
-public class Evento {
-	
-	private String codEvento;
-	private String nombreEvento;
-	
-	public String getCodEvento() {
-		return codEvento;
+public class Evento implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	private String codigo;
+	private String nombre;
+	private Date fecha;
+	private String tipo;
+	private String estado;
+	private ArrayList<Persona> misParticipantes;
+	private ArrayList<TrabajoCientifico> misTrabajosCientificos;
+	private ArrayList<Comision> misComisiones;
+	private ArrayList<Recurso> misRecursos;
+    public static int codigoEvento = 1;
+
+	public Evento(String nombre, Date fecha, String tipo) {
+		this.codigo = "EVENTO-N"+codigoEvento;
+		this.nombre = nombre;
+		this.fecha = fecha;
+		this.tipo = tipo;
+		this.estado = "Preparandose";
+		this.misParticipantes = new ArrayList<Persona>();
+		this.misTrabajosCientificos = new ArrayList<TrabajoCientifico>();
+		this.misComisiones = new ArrayList<Comision>();
+		this.misRecursos = new ArrayList<Recurso>();
+		codigoEvento++;
 	}
-	public void setCodEvento(String codEvento) {
-		this.codEvento = codEvento;
+
+	public String getCodigo() {
+		return codigo;
 	}
-	public String getNombreEvento() {
-		return nombreEvento;
+
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
 	}
-	public void setNombreEvento(String nombreEvento) {
-		this.nombreEvento = nombreEvento;
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public Date getFecha() {
+		return fecha;
+	}
+
+	public void setFecha(Date fecha) {
+		this.fecha = fecha;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+
+	public ArrayList<Persona> getMisParticipantes() {
+		return misParticipantes;
+	}
+
+	public void setMisParticipantes(ArrayList<Persona> misParticipantes) {
+		this.misParticipantes = misParticipantes;
+	}
+
+	public ArrayList<TrabajoCientifico> getMisTrabajosCientificos() {
+		return misTrabajosCientificos;
+	}
+
+	public void setMisTrabajosCientificos(ArrayList<TrabajoCientifico> misTrabajosCientificos) {
+		this.misTrabajosCientificos = misTrabajosCientificos;
+	}
+
+	public ArrayList<Comision> getMisComisiones() {
+		return misComisiones;
+	}
+
+	public void setMisComisiones(ArrayList<Comision> misComisiones) {
+		this.misComisiones = misComisiones;
+	}
+
+	public ArrayList<Recurso> getMisRecursos() {
+		return misRecursos;
+	}
+
+	public void setMisRecursos(ArrayList<Recurso> misRecursos) {
+		this.misRecursos = misRecursos;
+	}
+
+	public static int getCodigoEvento() {
+		return codigoEvento;
+	}
+
+	public static void setCodigoEvento(int codigoEvento) {
+		Evento.codigoEvento = codigoEvento;
 	}
 
 }
--- ProyectoFinal/src/logico/Jurado.java
+++ ProyectoFinal/src/logico/Jurado.java
@@ -1,11 +1,17 @@
 package logico;
 
-public class Jurado extends Persona {
-    private String especialidad;
+import java.io.Serializable;
 
-    public Jurado(String codigo, String nombre, String especialidad) {
-        super(codigo, nombre);
+public class Jurado extends Persona implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+    private String especialidad;
+    public static int codigoJurado = 1;
+
+    public Jurado(String nombre, String especialidad) {
+        super("JURADO-N"+codigoJurado, nombre);
         this.especialidad = especialidad;
+        codigoJurado++;
     }
 
 	public String getEspecialidad() {
@@ -14,6 +20,14 @@ public class Jurado extends Persona {
 
 	public void setEspecialidad(String especialidad) {
 		this.especialidad = especialidad;
+	}
+
+	public static int getCodigoJurado() {
+		return codigoJurado;
+	}
+
+	public static void setCodigoJurado(int codigoJurado) {
+		Jurado.codigoJurado = codigoJurado;
 	}
 
    
